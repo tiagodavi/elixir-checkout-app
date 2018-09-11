8b9c08b42202ded7e9d7fe2d4994d229c0d5e583
Tiago <tiago@manticore.local>
2018-09-11 12:00:19 -0300

    initial commit

---
+++ lib/store/application.ex
@@ -0,0 +1,20 @@
+defmodule Store.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Store.Worker.start_link(arg)
+      # {Store.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Store.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
